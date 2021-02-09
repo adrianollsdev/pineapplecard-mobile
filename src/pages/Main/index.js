69174f81f8f85e2df342f6b59f98625be9c44798
Adriano Leão <adrianolls.dev@gmail.com>
2021-02-09 03:40:46 -0300

    header invoice up

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {
     Text,
     View,
-    SafeAreaView
+    SafeAreaView,
+    FlatList
 } from 'react-native';
 
 import Carousel from 'react-native-snap-carousel';
@@ -17,72 +18,126 @@ export default class App extends React.Component {
         super(props);
         this.state = {
             activeIndex: 0,
-            carouselItems: [
-                {
-                    title: "R$ 2.000,00",
-                    text: "Vencimento 15 JAN",
-                },
-                {
-                    title: "R$ 2.000,00",
-                    text: "Vencimento 15 FEV",
-                },
-                {
-                    title: "R$ 2.000,00",
-                    text: "Vencimento 15 MAR",
-                },
-                {
-                    title: "R$ 2.000,00",
-                    text: "Vencimento 15 ABR",
-                },
-                {
-                    title: "R$ 2.000,00",
-                    text: "Vencimento 15 MAI",
-                },
-            ]
+            carouselItems: [],
+            loading: 0,
+            page: 0,
+            total: 0,
+            responseHeader: [{}],
+            payments: [],
+
         }
     }
+    
+    componentWillMount() {
+        this.loadHeadInvoices();
+    }
+
+    loadHeadInvoices() {
+        
+
+        api.get('profile/1').then(response => {
+            this.setState({
+                carouselItems: response.data.headers,
+                responseHeader: response.data,
+                loading: false
+            })
+            this.state.carouselItems.forEach(function (element, index) {
+                console.log('index', index)
+                console.log('element.invoiceType', element.invoiceType)
+                if (element.invoiceType == 'closed') {
+                    return this.setState({indexHome:index});
+                } else if (element.invoiceType == 'open' && this.state.indexHome == 0) {
+                    return this.setState({ indexHome: index });
+                }
+            }
+            )
+        });
+    }
+
+    loadListInvoices(monthYearRef) {
+
+
+        api.get('/payment/1/' + monthYearRef).then(response => {
+            this.setState({
+                payments: response.data.headers,
+                responseList: response.data
+            })
+            console.log("responseList: ", this.state.responseList);
+        });
+    }
+
 
     _renderItem({ item, index }) {
         return (
             <View style={styles.card} >
-                <View style={styles.headerCard}>
-                    <Text style={styles.titleCard}>{item.title}</Text>
-                    <Text style={styles.textCard}>{item.text}</Text>
+                <View style={ styles[item.invoiceType] }>
+                    <Text  style={styles.titleCard}>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.totalInvoice)}</Text>
+                    <Text style={styles.textCard}>Vencimento {item.dueDate}</Text>
                 </View>
+                {/* <FlatList
+                    style={styles.paymentList}
+                    data={this.state.payments}
+                    keyExtractor={payment => String(payment.id)}
+                    showsVerticalScrollIndicator={false}
+                    onEndReached={this.loadPayments()}
+                    onEndReachedThreshold={0.2}
+                    renderItem={({ item: payment }) => (
+                        {<View style={styles.payment}>
+                            <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+                            <Text style={styles.incidentValue}>{incident.name}</Text>
+
+                            <Text style={styles.incidentProperty}>CASO:</Text>
+                            <Text style={styles.incidentValue}>{incident.title}</Text>
+
+                            <Text style={styles.incidentProperty}>VALOR:</Text>
+                            <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</Text>
+
+                            <TouchableOpacity
+                                style={styles.detailsButton}
+                                onPress={() => navigateToDetail(incident)}
+                            >
+                                <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                                <Feather name="arrow-right" size={16} color="#E01041" />
+                            </TouchableOpacity>
+                        </View>}
+                    )}
+                /> */}
             </View>
         )
     }
 
-    render() {
-        async function headerCard() {
-            const response = await api.get('/profile', {
-                parms: 1
-            });
+    _renderItemList(){
+        return(
+            <View></View>
+        );
+    }
 
-            return (response.data)
-        }
-        return (          
-            
+    render() {
+
+        
+        
+        return (
+
             <View style={styles.container}>
                 <View style={styles.header}>
-                    <Logotipo style={styles.logo} />
+                    <Logotipo  style={styles.logo} />
                 </View>
-                <SafeAreaView style={styles.areaCard}>
+                <SafeAreaView  style={styles.areaCard}>
                     <View style={styles.viewCard}>
                         <Carousel
                             layout={"default"}
-                            ref={ref => this.carousel = ref}
+                            ref={(c) => {this._carousel = c}}
                             data={this.state.carouselItems}
                             sliderWidth={380}
-                            itemWidth={380}
+                            itemWidth={365}
                             renderItem={this._renderItem}
                             activeSlideAlignment={'end'}
                             onSnapToItem={index => this.setState({ activeIndex: index })}
-                            firstItem={1} />
+                            firstItem={3}
+                        />
                     </View>
                 </SafeAreaView>
             </View>
         );
     }
 }
-
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -38,6 +38,22 @@ export default StyleSheet.create({
     },
 
     headerCard: {
+        "top": 54,
+        "left": 0,
+        "width": 375,
+        "height": 75,
+        "backgroundColor": "#F8B600",
+        "shadowOffset": {
+            "width": 0,
+            "height": 10
+        },
+        "shadowRadius": 15,
+        "shadowColor": "#00000029",
+        "shadowOpacity": 1,
+        "opacity": 1
+    },
+
+    paid: {
         left: 0,
         height: 75,
         justifyContent: 'center',
@@ -52,9 +68,39 @@ export default StyleSheet.create({
         elevation: 3,
     },
 
+    closed: {
+        left: 0,
+        height: 75,
+        justifyContent: 'center',
+        backgroundColor: '#F42C2C',
+        shadowColor: "#0000000F",
+        shadowOffset: {
+            width: 10,
+            height: 15,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 4.65,
+        elevation: 3,
+    },
+
+    open: {
+        left: 0,
+        height: 75,
+        justifyContent: 'center',
+        backgroundColor: '#F8B600',
+        shadowColor: "#0000000F",
+        shadowOffset: {
+            width: 10,
+            height: 15,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 4.65,
+        elevation: 3,
+    },
+
 
     titleCard: {
-        flex:1,
+        flex: 1,
         color: '#FFFFFF',
         fontFamily: 'Roboto',
         fontSize: 30,
@@ -65,7 +111,7 @@ export default StyleSheet.create({
     },
 
     textCard: {
-        flex:1,
+        flex: 1,
         fontFamily: 'Roboto',
         fontSize: 15,
         letterSpacing: 0,
@@ -84,6 +130,101 @@ export default StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'center',
+    },
+
+    paymentList: {
+        backgroundColor: '#FFFFFF',
+        flex:1,
+    },
+
+    lineList: {
+        "top": 158,
+        "left": 27,
+        "width": 0,
+        "height": 864,
+        "borderWidth": 1,
+        "borderColor": "#D3CFCF",
+        "borderStyle": "solid",
+        "opacity": 1
+    },
+
+    hourList: {
+        "top": 153,
+        "left": 304,
+        "width": 43,
+        "height": 1129,
+        "textAlign": "right",
+        "fontStyle": "normal",
+        "fontWeight": "300",
+        "fontVariant": [],
+        "fontSize": 10,
+        "fontFamily": "Roboto",
+        "lineHeight": 86,
+        "letterSpacing": 0,
+        "color": "#707070",
+        "opacity": 1
+    },
+
+    valueList: {
+        "top": 174,
+        "left": 52,
+        "width": 121,
+        "height": 1129,
+        "textAlign": "left",
+        "fontStyle": "normal",
+        "fontWeight": "300",
+        "fontVariant": [],
+        "fontSize": 12,
+        "fontFamily": "Roboto",
+        "lineHeight": 86,
+        "letterSpacing": 0,
+        "color": "#707070",
+        "opacity": 1
+    },
+
+    elipseOpenInvoice: {
+        "top": 151,
+        "left": 19,
+        "width": 15,
+        "height": 15,
+        "backgroundColor": "#F8B600",
+        "opacity": 1
+    },
+
+    elipsePayment: {
+        "top": 238,
+        "left": 21,
+        "width": 11,
+        "height": 11,
+        "backgroundColor": "#3AEA63",
+        "opacity": 1
+    },
+
+    iconReversal: {
+        "top": 405,
+        "left": 12,
+        "width": 28,
+        "height": 25,
+        "backgroundColor": "#FFFFFF",
+        "opacity": 1
+    },
+
+    comentPayment: {
+        "top": 449,
+        "left": 52,
+        "width": 121,
+        "height": 1129,
+        "textAlign": "left",
+        "fontStyle": "normal",
+        "fontWeight": "300",
+        "fontVariant": [],
+        "fontSize": 12,
+        "fontFamily": "Roboto",
+        "lineHeight": 86,
+        "letterSpacing": 0,
+        "color": "#F8B600",
+        "opacity": 1
     }
+
 
 });
